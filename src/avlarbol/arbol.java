fffdb8003618779f9408e30d1ab91961d1f3aafd
josevang0811 <josevang0811@gmail.com>
2026-08-19 18:33:02 -0500

    tercera parte

--- src/avlarbol/arbol.java
+++ src/avlarbol/arbol.java
@@ -9,5 +9,90 @@ package avlarbol;
  * @author sncardenas
  */
 public class arbol {
+    private nodo raiz;
+    public arbol() {
+        this.raiz = null;
+    }
+    public static int getcol(int h) {
+        if (h == 1) {
+            return 1;
+        }
+        return getcol(h - 1) + getcol(h - 1) + 1;
+    }
     
+    public static int alturaArbol(nodo n1) {
+        if (n1 == null) {
+            return 0;
+        }
+        return Math.max(alturaArbol(n1.getIzquierdo()), alturaArbol(n1.getDerecho())) + 1;
+    }
+      
+    public static void printTree(int[][] M, nodo root, int col, int row, int height) {
+        if (root == null) {
+            return;
+        }
+        M[row][col] = (char) root.getValor();
+        printTree(M, root.getIzquierdo(), col - (int) Math.pow(2, height - 2), row + 1, height - 1);
+        printTree(M, root.getDerecho(), col + (int) Math.pow(2, height - 2), row + 1, height - 1);
+    }
+    public void TreePrinter() {
+        int h = alturaArbol(this.raiz);
+        int col = getcol(h);
+        int[][] M = new int[h][col];
+        printTree(M, this.raiz, col / 2, 0, h);
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < col; j++) {
+                if (M[i][j] == 0) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print(M[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+    public int alturaNodo(nodo nodo){
+        if(nodo==null){
+            return 0;
+        }
+        return nodo.getAltafulla();
+    }
+    public int factordequilibrio(nodo nodo){
+        if (nodo==null){
+            return 0;
+        }
+        return alturaNodo(nodo.getIzquierdo())-alturaNodo(nodo.getDerecho());
+    }
+    public nodo rotacionderecha(nodo y){
+        nodo x=y.getIzquierdo();
+        nodo temp=y.getDerecho();
+        x.setDerecho(y);
+        y.setIzquierdo(temp);
+        y.setAltafulla(Math.max(alturaNodo(y.getIzquierdo()), alturaNodo(y.getDerecho()))+1);
+        x.setAltafulla(Math.max(alturaNodo(x.getIzquierdo()), alturaNodo(x.getDerecho()))+1);
+        return x;
+    }
+    public nodo rotacionizquierda(nodo x){
+        nodo y=x.getDerecho();
+        nodo temp=x.getIzquierdo();
+        y.setIzquierdo(x);
+        x.setDerecho(temp);
+        x.setAltafulla(Math.max(alturaNodo(x.getIzquierdo()), alturaNodo(x.getDerecho()))+1);
+        y.setAltafulla(Math.max(alturaNodo(y.getIzquierdo()), alturaNodo(y.getDerecho()))+1);
+        return x;
+    }
+    public void insetar(int valor){
+        this.raiz=insertar2(this.raiz,valor);
+    }
+    
+    public nodo insertar2(nodo nodo,int valor){
+        if(nodo==null){
+            nodo nuevonodo=new nodo(valor);
+            return nuevonodo;
+        }
+        if(valor<nodo.getValor()){
+            nodo.setIzquierdo(nodo);
+        }
+    }
 }
--- src/avlarbol/nodo.java
+++ src/avlarbol/nodo.java
@@ -9,5 +9,31 @@ package avlarbol;
  * @author sncardenas
  */
 public class nodo {
-    
+    private int valor;
+    private nodo izquierdo;
+    private nodo derecho;
+    private int altafulla;
+
+    public nodo(int valor) {
+        this.valor = valor;
+        this.izquierdo = null;
+        this.derecho = null;
+        this.altafulla = 1;
+    }
+
+    public int getValor() { return valor; }
+    public void setValor(int valor) { this.valor = valor; }
+
+    public nodo getIzquierdo() { return izquierdo; }
+    public void setIzquierdo(nodo izquierdo) { this.izquierdo = izquierdo; }
+
+    public nodo getDerecho() { return derecho; }
+    public void setDerecho(nodo derecho) { this.derecho = derecho; }
+
+    public int getAltafulla() { return altafulla; }
+    public void setAltafulla(int altafulla) { this.altafulla = altafulla; }
+
+    public boolean esHoja() {
+        return izquierdo == null && derecho == null;
+    }
 }
